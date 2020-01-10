95527347d489561703a8d06d8d8a995d1e71f0f5
AC-29 <56473325+AC-29@users.noreply.github.com>
2020-01-10 14:38:49 +0100

    Adding projections

--- paper_kernel.py
+++ paper_kernel.py
@@ -4,9 +4,12 @@ import sys
 class extension_cluster_kernel:
     def __init__(self, data, type, parameters=None):
         self.dict = {}
+        self.data = data
         for i,d in enumerate(data):
             self.dict[str(d)] = i
         K = rbf_kernel(data)
+        self.rbfK = K
+        self.invRbfK = np.linalg.inv(self.rbfK)
         D = np.zeros((K.shape[0],K.shape[1]))
         diagonalElements = np.sum(K,axis = 1)
         for i in range(D.shape[0]):
@@ -70,12 +73,36 @@ class extension_cluster_kernel:
             ind1 = self.dict[str(x1)]
             gram_matrix[i, :] = self.K[ind1,index2list]
         return gram_matrix
+    def distanceGeneral(self,X1,X2):
+        gram_matrix = np.zeros((X1.shape[0], X2.shape[0]))
+        index2list = np.zeros((X2.shape[0]),dtype=int)
+        for j, x2 in enumerate(X2):
+            if str(x2) in self.dict:
+                index2list[j] = self.dict[str(x2)]
+            else:
+                return self.distance_test(X1,X2)
+        for i, x1 in enumerate(X1):
+            if str(x1) in self.dict:
+                ind1 = self.dict[str(x1)]
+                gram_matrix[i, :] = self.K[ind1,index2list]
+            else:
+                return self.distance_test(X2,X1).T
+        return gram_matrix
     def distance_test(self,X1,X2):
         index1list = np.zeros((X1.shape[0]),dtype=int)
         for j, x1 in enumerate(X1):
             index1list[j] = self.dict[str(x1)]
-        known_samples = data[index1list]
+        known_samples = self.data[index1list]
         new_samples = X2
+        V = rbf_kernel(new_samples,known_samples)
+        K_hat_cut = np.zeros((len(index1list),len(index1list)))
+        invRbfK_cut = np.zeros((len(index1list),len(index1list)))
+        for i,ei in enumerate(index1list):
+            for j,ej in enumerate(index1list):
+                K_hat_cut[i,j] = self.K[ei,ej]
+                invRbfK_cut[i,j] = self.invRbfK[ei,ej]
+        projection = ((K_hat_cut).dot(invRbfK_cut)).dot(V.T)
+        return projection
         
         
 class random_walk:
