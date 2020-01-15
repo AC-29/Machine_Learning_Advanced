13ffbc9c5944a2f988d2d89fecc58d9a719783cb
AC-29 <56473325+AC-29@users.noreply.github.com>
2020-01-15 19:16:36 +0100

    Update gettingCTG.py

--- CTG_Dataset/gettingCTG.py
+++ CTG_Dataset/gettingCTG.py
@@ -4,6 +4,7 @@ from sklearn import svm
 from TSVM import *
 from paper_kernel import *
 import pandas as pd
+from sklearn.metrics import f1_score
 
 with open('CTG_data.csv', 'r') as f:
     reader = csv.reader(f, delimiter=';')
@@ -23,25 +24,27 @@ pand = pd.DataFrame(data)
 pand = pand.drop_duplicates()
 data = pand.to_numpy()
 
+plus = data[:,:1645]
+minus = data[:,1645:]
 targets=data[:,-1]
 data=data[:,:-1]
 
 sigma = 6
 gamma = 1/(2*(sigma**2))
+
 trainSize = 40
 iterations = 50
 accuracy_total = np.zeros((iterations, 4))
 count = 0
 for i in range(iterations):
     #
-    # lengthTrain = 1
+    lengthTrain = 1
     
-    # while lengthTrain == 1:
+    while lengthTrain == 1:
         # np.random.seed(i+count)
-    np.random.seed(i)
-    indeces = np.random.permutation(data.shape[0])
-    train_targets = targets[indeces[0:trainSize]]
-    lengthTrain = len(np.unique(train_targets))
+        indeces = np.random.permutation(data.shape[0])
+        train_targets = targets[indeces[0:trainSize]]
+        lengthTrain = len(np.unique(train_targets))
         # count += count
         
     train_l = data[indeces[:trainSize]]    
@@ -52,50 +55,69 @@ for i in range(iterations):
         
 
     # ## SVM ##
-    # clf = svm.SVC(C=100, gamma=gamma,kernel = 'linear')
-    # clf.fit(train_l, train_targets)
-    # accuracy_total[i, 0] = clf.score(test_data, test_data_target)
+    clf = svm.SVC(C=100, gamma=gamma,kernel = 'rbf')
+    clf.fit(train_l, train_targets)
+    prediction = clf.predict(test_data)
+    f1SVM = f1_score(test_data_target,prediction,average = 'weighted')
+    accuracy_total[i, 0] = clf.score(test_data, test_data_target)
+    print("UNIQUE SVM: ")
+    print(np.unique(prediction))
     
     
     # ## TSVM ##
-    # C = 100
-    # kernel = 'linear'
-    # model = TSVM(kernel, gamma, C)
-    # model.train(train_l, train_targets, train_unlabeled)
-    # test_predictions = model.predict(test_data)
-    # accuracy_total[i, 1] = compute_accuracy(test_predictions, test_data_target)
+    C = 100
+    kernel = 'rbf'
+    model = TSVM(kernel,gamma,C)
+    model.train(train_l, train_targets, train_unlabeled)
+    test_predictions = model.predict(test_data)
+    f1TSVM = f1_score(test_data_target,test_predictions,average = 'weighted')
+    accuracy_total[i, 1] = compute_accuracy(test_predictions, test_data_target)
+    print("UNIQUE TSVM: ")
+    print(np.unique(test_predictions))
     
     trn_labeled = train_l
     trn_unlabeled = train_unlabeled
     trn = np.concatenate((trn_labeled, trn_unlabeled), axis=0)
     
-    ## Extension Cluster Kernel ##
-    # dataAux = np.concatenate((trn, test_data), axis=0)
-    # kernel = kernel_function('rbf')
-    # lin_ker = extension_cluster_kernel(dataAux, kernel,'linear')
-    # eig = lin_ker.eigvalues
-    # cut_off = k_th_largest_eig(eig, i)
-    # lin_ker.poly_step([cut_off,1/2,2])
-    # # lin_ker.polynomial(12)
-    # # lin_ker.step(cut_off)
-    # clf_ck = svm.SVC(kernel=lin_ker.distance, C=100, class_weight='balanced')
-    # trn_target = np.array(train_targets)
-    # clf_ck.fit(trn_labeled, trn_target)
-    # accuracy_total[i, 2] = clf_ck.score(test_data, test_data_target)
-    # print(accuracy_total[i,2])
+    # Extension Cluster Kernel ##
+    dataAux = np.concatenate((trn, test_data), axis=0)
+    kernel = kernel_function('rbf',gamma = gamma)
+    lin_ker = extension_cluster_kernel(dataAux, kernel,'polynomial',12)
+    eig = lin_ker.eigvalues
+    trn_target = np.array(train_targets)
+    # JH = JH_bound(trn_labeled,trn_target,test_data,gamma)
+    # print(JH)
+    # cut_off = k_th_largest_eig(eig, 10)
+    # lin_ker.poly_step([cut_off,1/2,1])
+    # lin_ker.step(cut_off)
+    lin_ker.polynomial(12)
+    clf_ck = svm.SVC(kernel=lin_ker.distance, C=100, class_weight='balanced')
+    
+    clf_ck.fit(trn_labeled, trn_target)
+    prediction = clf.predict(test_data)
+    f1CK = f1_score(test_data_target,prediction,average = 'weighted')
+    accuracy_total[i, 2] = clf_ck.score(test_data, test_data_target)
+    print("UNIQUE CK: ")
+    print(np.unique(prediction))
+    print(accuracy_total[i,2])
     
     
     # #RW
     unlabeled_data = np.concatenate((train_unlabeled,test_data))
-    unlabeled_targets = np.concatenate((train_targets,test_data_target))
+    unlabeled_targets = np.concatenate((train_unlabeled_target,test_data_target))
     
-    rw = random_walk(train_l,train_targets,unlabeled_data,gam = 0.27, k = 7, t = 5)
-    # accuracy_total[i,3] = np.sum(1*(rw.results[len(train_targets):] == unlabeled_targets))/len(unlabeled_targets)
-    aux = 0
-    for j in range(len(train_targets)):
-        aux+=(rw.results[len(train_targets)+j] ==int(unlabeled_targets[j]))*1
+    rw = random_walk(train_l,train_targets,unlabeled_data,gam = 0.027, k = 5, t = 4)
+    accuracy_total[i,3] = np.sum(1*(rw.results[len(train_targets):] == unlabeled_targets))/len(unlabeled_targets)
+    prediction = rw.results
+    labels_all = np.concatenate((train_targets,test_data_target))
+    f1RW = f1_score(labels_all,prediction,average = 'weighted')
+    print("UNIQUE RW: ")
+    print(np.unique(rw.results))
+    # aux = 0
+    # for j in range(len(train_targets)):
+    #     aux+=(rw.results[len(train_targets)+j] ==int(unlabeled_targets[j]))*1
     
-    accuracy_total[i,3] = aux/len(unlabeled_targets)
+    # accuracy_total[i,3] = aux/len(unlabeled_targets)
     print(accuracy_total[i,3])
     
     
